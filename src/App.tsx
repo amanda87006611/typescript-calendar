461291122fd02e70ae6ec1b391558bb463a14846
amanda <amanda87006611@gmail.com>
2023-05-11 18:10:44 +0800

    Remove browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
-import TodoPlatform from "./TodoPlatform";
+// import TodoPlatform from "./TodoPlatform";
 
 import store from "./store";
+import Todolist from "./Components/Todolist";
 
 // 基本type的定義方法筆記寫於Notion - react typescript
 
@@ -12,9 +13,10 @@ const App: React.FC = () => {
   return (
     <div className='APP'>
       <Provider store={store}>
-        <BrowserRouter>
-          <TodoPlatform />
-        </BrowserRouter>
+        {/* <BrowserRouter> */}
+        {/* <TodoPlatform /> */}
+        {/* </BrowserRouter> */}
+        <Todolist />
       </Provider>
     </div>
   );
